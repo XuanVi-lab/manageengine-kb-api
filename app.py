650063ec4fa6c24b46c8cad7f48c72f0e80b7f4b
XuanVi <xuanvi@example.com>
2025-05-27 13:46:20 +0700

    Nội dung thay đổi

--- app.py
+++ app.py
@@ -29,5 +29,7 @@ def scrape():
         return jsonify({"status": "error", "message": str(e)})
 
 if __name__ == "__main__":
-    print("API running on http://0.0.0.0:5000/scrape?url=https://...")
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    print(f"API running on http://0.0.0.0:{port}/scrape?url=https://...")
+    app.run(debug=True, host="0.0.0.0", port=port)
